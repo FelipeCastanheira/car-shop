7d07496758ff873ba46fb3de9340334a3a2cd96f
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-30 09:02:23 -0300

    car model

---
+++ src/models/Car.ts
@@ -0,0 +1,27 @@
+import { Schema, model as createModel } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const stringObj = { type: String, required: true };
+const numberObj = { type: Number, required: true };
+const boolObj = { type: Boolean, required: false };
+
+const carSchema = new Schema<ICar>({
+  status: boolObj,
+  model: stringObj,
+  color: stringObj,
+  year: numberObj,
+  buyValue: numberObj,
+  doorsQty: numberObj,
+  seatsQty: numberObj,
+});
+
+class Car extends MongoModel<ICar> {
+  constructor(model = createModel('Car', carSchema)) {
+    super(model);
+  }
+}
+
+// const carModel = model<ICar>('Car', carSchema);
+
+export default Car;
