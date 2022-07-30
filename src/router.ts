ae539a52e26acea9a07cedd27880ae4506cc7fab
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-30 14:34:33 -0300

    req4

--- src/router.ts
+++ src/router.ts
@@ -10,7 +10,7 @@ const carModel = new Car();
 const carService = new CarService(carModel);
 const carController = new CarController(carService);
 
-route.post('/cars', validCar, carController.create);
+route.post('/cars', validCar, (req, res) => carController.create(req, res));
 // route.get('/car/:id', carController.readOne);
 // route.put('/car/:id', carController.update);
 
