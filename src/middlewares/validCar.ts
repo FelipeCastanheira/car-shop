d11e89d58f8cc72fcc0eba8efe6261d3c42a5d15
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-30 11:25:29 -0300

    car body validation

---
+++ src/middlewares/validCar.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+import { CarZod } from '../interfaces/ICar';
+
+const validCar = (req: Request, res: Response, next: NextFunction) => {
+  const parsed = CarZod.safeParse(req.body);
+  if (parsed.success) return next();
+  return res.status(400).send();
+};
+
+export default validCar;
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import CarController from './controllers/CarController';
+import validCar from './middlewares/validCar';
 import Car from './models/Car';
 import CarService from './services/CarService';
 
@@ -9,8 +10,8 @@ const carModel = new Car();
 const carService = new CarService(carModel);
 const carController = new CarController(carService);
 
-route.post('/cars', (req, res) => carController.create(req, res));
-// route.get('/car/:id', (req, res) => carController.readOne(req, res));
-// route.put('/car/:id', (req, res) => carController.update(req, res));
+route.post('/cars', validCar, carController.create);
+// route.get('/car/:id', carController.readOne);
+// route.put('/car/:id', carController.update);
 
 export default route;
