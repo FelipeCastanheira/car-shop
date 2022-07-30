9392ea53a3b656ed3ac4c18327fe0e58a30a3fd8
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-30 20:05:32 -0300

    test: service

--- src/tests/unit/services/carService.test.ts
+++ src/tests/unit/services/carService.test.ts
@@ -1,22 +1,53 @@
-// template para criação dos testes de cobertura da camada de service
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ZodError } from 'zod';
+import CarModel from '../../../models/Car';
+import CarService from '../../../services/CarService';
+import { carMock, carMockWithId } from '../../mocks/carMock';
 
+describe('car Service', () => {
+	const carModel = new CarModel();
+	const carService = new CarService(carModel);
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+	before(() => {
+		sinon.stub(carModel, 'create').resolves(carMockWithId);
+		sinon.stub(carModel, 'readOne')
+			.onCall(0).resolves(carMockWithId) 
+			.onCall(1).resolves(null); 
+	})
+	after(() => {
+		sinon.restore()
+	})
+	describe('Create car', () => {
+		it('Success', async () => {
+			const carCreated = await carService.create(carMock);
 
-// describe('Sua descrição', () => {
+			expect(carCreated).to.be.deep.equal(carMockWithId);
+		});
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+		it('Failure', async () => {
+			try {
+				// o "as any"(casting) abaixo pois o create não aceita um parâmetro inválido
+				await carService.create({} as any);
+			} catch (error) {
+				expect(error).to.be.instanceOf(ZodError);
+			}
+		});
+	});
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+	describe('ReadOne car', () => {
+		it('Success', async () => {
+			const carCreated = await carService.readOne(carMockWithId._id);
 
-//   it('', async () => {});
+			expect(carCreated).to.be.deep.equal(carMockWithId);
+		});
 
-// });
+		it('Failure', async () => {
+			try {
+				await carService.readOne(carMockWithId._id);
+			} catch (error:any) {
+				expect(error.message).to.be.a('string');
+			}
+		});
+	});
+});
