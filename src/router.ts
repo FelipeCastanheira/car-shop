a32e404d46f5b3350fcffde86f658711ebf88ccf
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-30 09:33:34 -0300

    fix route path

--- src/router.ts
+++ src/router.ts
@@ -9,7 +9,7 @@ const carModel = new Car();
 const carService = new CarService(carModel);
 const carController = new CarController(carService);
 
-route.post('/car', (req, res) => carController.create(req, res));
+route.post('/cars', (req, res) => carController.create(req, res));
 // route.get('/car/:id', (req, res) => carController.readOne(req, res));
 // route.put('/car/:id', (req, res) => carController.update(req, res));
 
