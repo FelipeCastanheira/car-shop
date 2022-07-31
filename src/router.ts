2e8de7f4210ad6d04a023eff8038215b1704a7dc
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-31 10:26:26 -0300

    motorcycle routes

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import CarController from './controllers/CarController';
+import MotorcycleController from './controllers/MotorcycleController';
 import validCar from './middlewares/validCar';
 import validId from './middlewares/validId';
+import validMotorcycle from './middlewares/validMotorcycle';
 import Car from './models/Car';
+import Motorcycle from './models/Motorcycle';
 import CarService from './services/CarService';
+import MotorcycleService from './services/MotorcycleService';
 
 const route = Router();
 
@@ -24,6 +28,35 @@ route.delete(
   '/cars/:id',
   validId,
   (req, res) => carController.delete(req, res),
+);
+
+const motorcycleModel = new Motorcycle();
+const motorcycleService = new MotorcycleService(motorcycleModel);
+const motorcycleController = new MotorcycleController(motorcycleService);
+
+const pathByMotoId = '/motorcycles/:id';
+
+route.post(
+  '/motorcycles',
+  validMotorcycle,
+  (req, res) => motorcycleController.create(req, res),
+);
+route.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
+route.get(
+  pathByMotoId,
+  validId,
+  (req, res) => motorcycleController.readOne(req, res),
+);
+route.put(
+  pathByMotoId,
+  validId,
+  validMotorcycle,
+  (req, res) => motorcycleController.update(req, res),
+);
+route.delete(
+  pathByMotoId,
+  validId,
+  (req, res) => motorcycleController.delete(req, res),
 );
 
 export default route;
